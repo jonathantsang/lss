a1eea945009466b3ca0c3997f2fd88217cfd1332
jonathantsang <j26tsang@gmail.com>
2019-04-30 22:43:34 -0700

    casino roulette

--- Assets/Scripts/StatsPageController.cs
+++ Assets/Scripts/StatsPageController.cs
@@ -5,18 +5,19 @@ using UnityEngine.UI;
 
 public class StatsPageController : MonoBehaviour {
 
-	GameObject stats;
+	// GameObject stats;
 	DataController dataController;
 
 	// Use this for initialization
 	void Start () {
 		dataController = GameObject.FindGameObjectWithTag ("DataController").GetComponent<DataController> ();
-		stats = GameObject.FindGameObjectWithTag ("Stats");
-		Text money = stats.transform.GetChild (0).GetComponent<Text> ();
-		money.text = "Total Money: " + dataController.getTotalMoney ().ToString ();
+		// TODO fix
+		//Text money = stats.transform.GetChild (0).GetComponent<Text> ();
+		// money.text = "Total Money: " + dataController.getTotalMoney ().ToString ();
 	
-		Text clicks = stats.transform.GetChild (1).GetComponent<Text> ();
-		clicks.text = "Total Clicks: " + dataController.getTotalClicks ().ToString ();
+		// stats = GameObject.FindGameObjectWithTag ("Stats");
+		//Text clicks = stats.transform.GetChild (1).GetComponent<Text> ();
+		//clicks.text = "Total Clicks: " + dataController.getTotalClicks ().ToString ();
 	}
 	
 	// Update is called once per frame
